dfc4bb389de291a1f0bf81a6db3815aa883526a7
YiJiangFengYun <yijiangfengyun@outlook.com>
2019-08-20 15:12:30 +0800

    Development

--- src/renderer/model/index.ts
+++ src/renderer/model/index.ts
@@ -1,3 +1,15 @@
+import * as formatText from "format_text";
+
 export function init() {
     return Promise.resolve();
 }
+
+export class Model {
+    public formatText: formatText.FormatText = new formatText.FormatText();
+
+    public constructor() {
+
+    }
+}
+
+export const model = new Model();
--- src/renderer/view/edit.tsx
+++ src/renderer/view/edit.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as modFormatText from "format_text";
 import * as model from "../model";
 
 const fontSizes = [6, 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72];
@@ -7,6 +8,8 @@ export class Edit extends React.Component<{}, {}> {
 
     private _color: string = "#ffffff";
     private _size: number = fontSizes[0];
+    private _lastTextHtml: string;
+    private _refTextArea: React.RefObject<HTMLDivElement> = React.createRef();
     public constructor(props: Readonly<{}>) {
         super(props);
 
@@ -30,9 +33,13 @@ export class Edit extends React.Component<{}, {}> {
                         fontSize: this._size,
                         verticalAlign: "top",
                     }}
+                    onFocus={this._onFocus.bind(this)}
                     onBlur={this._onBlur.bind(this)}
+                    ref={this._refTextArea}
                 >
-                    <span>Please input text here.</span>
+                    {
+                        this._toTextElements()
+                    }
                 </div>
                 <div
                     id="tool_area"
@@ -74,10 +81,11 @@ export class Edit extends React.Component<{}, {}> {
                                 verticalAlign: "top",
                             }}
                             onChange={this._onSizeChanged.bind(this)}
+                            value={this._size}
                         >
                             {
                                 fontSizes.map((value) => {
-                                    return <option value={ value } selected={ value === this._size }>{ value }</option>
+                                    return <option key={ value } value={ value }>{ value }</option>
                                 })
                             }
                         </select>
@@ -87,10 +95,111 @@ export class Edit extends React.Component<{}, {}> {
         )
     }
 
+    private _toTextElements() {
+        let formatText = model.model.formatText;
+        let data = formatText.data;
+        let text = data.text;
+        let formats = data.formats;
+        let formatCount = formats.length;
+        let brs = data.brs;
+        let brCount = brs.length;
+        let brStart = 0;
+
+        let result: JSX.Element[] = [];
+        result.length = formatCount;
+        for (let i = 0; i < formatCount; ++i) {
+            let format = formats[i];
+            let subBegin = format.begin;
+            let subEnd = format.end;
+            let subResult = "";
+            for (let j = brStart; j < brCount; ++j) {
+                let index = brs[j];
+                if (index < subEnd) {
+                    subResult += text.substring(subBegin, index);
+                    subResult += "<br/>"
+                    subBegin = index;
+                    brStart = j + 1;
+                } else {
+                    brStart = j;
+                    break;
+                }
+            }
+            subResult += text.substring(subBegin, subEnd);
+            let style: React.CSSProperties = {};
+            if (format.types & modFormatText.getFormatTypeBits(modFormatText.FormatType.COLOR)) {
+                style.color = `#${format.color.toString(16)}`;
+            }
+            if (format.types & modFormatText.getFormatTypeBits(modFormatText.FormatType.SIZE)) {
+                style.fontSize = `${format.size}px`;
+            }
+            if (format.types & modFormatText.getFormatTypeBits(modFormatText.FormatType.BOLD)) {
+                style.fontWeight = `bold`;
+            } else {
+                style.fontWeight = `normal`;
+            }
+            if (format.types & modFormatText.getFormatTypeBits(modFormatText.FormatType.ITALIC)) {
+                style.fontStyle = `italic`;
+            } else {
+                style.fontStyle = `normal`;
+            }
+            result[i] = <span key={i + 1} style={style}>{subResult}</span>;
+        }
+        return result;
+    }
+
+    private _fromTextElements() {
+        let children = this._refTextArea.current.children;
+        let formatText = model.model.formatText;
+        let datas: modFormatText.Data[] = [];
+        let len = children.length;
+        for (let i = 0; i < len; ++i) {
+            let element = children.item(i) as HTMLSpanElement;
+            if (! element.innerHTML) continue;
+            let data: modFormatText.Data = modFormatText.create();
+            let style = element.style;
+            let childNodes = element.children;
+            let childNodeCount = childNodes.length;
+            let text = "";
+            for (let j = 0; j < childNodeCount; ++j) {
+                let childNode = childNodes.item(j);
+                if (childNode.tagName == "BR") {
+                    modFormatText.addBR(data, text.length);
+                } else {
+                    text += childNode.nodeValue;
+                }
+            }
+            data.text = text;
+            let format: modFormatText.Format = { begin: 0, end: text.length, types: 0 };
+            modFormatText.addFormatColor(format, Number(`0x${style.color.slice(1)}`));
+            modFormatText.addFormatSize(format, Number(style.fontSize.slice(0, style.fontSize.length - 2)));
+            if (style.fontWeight === "bold") {
+                modFormatText.addFormats(format, modFormatText.FormatType.BOLD);
+            }
+            if (style.fontStyle === "italic") {
+                modFormatText.addFormats(format, modFormatText.FormatType.ITALIC);
+            }
+            datas.push(data);
+        }
+
+        len = datas.length;
+        if (len > 0) formatText.init(datas[0]);
+        for (let i = 1; i < len; ++i) {
+            formatText.append(datas[i]);
+        }
+    }
+
+    private _onFocus(event: FocusEvent) {
+        let target = event.target;
+        let innerHtml = (target as HTMLDivElement).innerHTML;
+        this._lastTextHtml = innerHtml;
+    }
+
     private _onBlur(event: FocusEvent) {
         let target = event.target;
         let innerHtml = (target as HTMLDivElement).innerHTML;
+        if (this._lastTextHtml !== innerHtml) {
 
+        }
     }
 
     private _onColorChanged(event: Event) {
